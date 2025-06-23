b4e40270ec8633c366199d917dc43e3d60de5eb1
Daniel Wijaya <dan.sam.wijaya@gmail.com>
2025-06-22 20:20:24 -0400

    Gitignore added

--- frontend/lib/supabaseClient.js
+++ frontend/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://uwqqbxpheiccvobrtcog.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cXFieHBoZWljY3ZvYnJ0Y29nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNzUyMDksImV4cCI6MjA1OTk1MTIwOX0.dEX0JXcA-dytiEQnKxPkt_bQmFtziEXDlPrfFWq2FiA'; // from Supabase → Project Settings → API
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
