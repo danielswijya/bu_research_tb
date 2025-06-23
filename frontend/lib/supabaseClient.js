f4727f26eb3284c186396ac1b3565a0357d4877d
Daniel Wijaya <dan.sam.wijaya@gmail.com>
2025-06-22 20:36:09 -0400

    Fixed .env file

--- frontend/lib/supabaseClient.js
+++ frontend/lib/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_KEY;
+console.log("✅ VITE_SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
